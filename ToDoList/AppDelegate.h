b1c174f7010c29bf9770a33046efa1f262cb80b0
user24877 <user24877@74-80-237-108.ic2net.net>
2014-10-07 23:02:27 -0700

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by user24877 on 10/7/14.
+//  Copyright (c) 2014 user24877. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
